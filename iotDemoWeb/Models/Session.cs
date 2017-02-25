400860594ed703c863e56e4d8e154ede187a7874
Mike Benkovich <mike@benko.com>
2017-02-25 10:20:01 -0600

    Bsse project

---
+++ iotDemoWeb/Models/Session.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iotDemoWeb.Models
+{
+    public class Session
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Instructor { get; set; }
+        public string Abstract { get; set; }
+        public DateTime StartDt { get; set; }
+
+    }
+}
